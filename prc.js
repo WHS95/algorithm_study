16a764b03a1a64c66ab1fd895f96dcbeb9d2c542
swh_95 <tjdngur22@gmail.com>
2022-11-19 12:06:38 +0900

    first commit

--- prc.js
+++ prc.js
@@ -1,12 +1,39 @@
-function solution(n) {
-  
-    let arr = n.toString().split("").reverse(); //[ '5', '4', '3', '2', '1' ]
-    let numarr = n.toString().split("").reverse().map(Number);
-    let sum = numarr.reduce((a, b) => a + b); //15
-  
-    let arr1 = arr.join("+"); //5+4+3+2+1
-    let result = `${arr1}=${sum}`;
-    return result
-  }
-  console.log(solution(12345));
-  
+// var iterable = [3, 5, 7];
+// iterable.foo = "hello";
+
+// for (var key in iterable) {
+//   console.log(key); // 0, 1, 2, "foo", "arrCustom", "objCustom"
+// }
+
+// for (var value of iterable) {
+//   console.log(value); // 3, 5, 7
+// }
+
+const person = { fname: "John", lname: "Doe", age: 25 };
+
+let txt = "";
+//object에서 키값을 가져온다.
+for (let x in person) {
+  txt += person[x] + " ";
+}
+
+console.log(txt);
+
+
+
+// const numbers = [45, 4, 9, 16, 25];
+
+// let txt1 = "";
+// //배열에서는 인덱스 값을 가져 온다.
+// for (let x in numbers) {
+//   txt1 += numbers[x];
+// }
+
+// console.log(txt1);
+
+
+
+// const object = { 1: "가", 2: "나", 3: "다", 4: "라" };
+// for (let z in object) {
+//   console.log(object[z]);
+// }
--- vs에서 진행하는법 copy.js
+++ vs에서 진행하는법 copy.js
@@ -1,28 +1,10 @@
+let bookArray = [
+    "자바스크립트 완벽하게 이해하기",
+    32000,
+    "훈이",
+    "자바스크립트짱짱",
+  ];
 
-// vs에서 진행하는법
+  console.log(bookArray[Symbol.iterator]());
 
-//값이 들어오는부분(프로그래머스 상에서
-    let a = 3;
-    let b = 5;
-
-    var answer = 0;
-    let num = [];
-    if (a === b) {
-        answer = a;
-    } else {
-            answer = a + b
-        }
-    console.log(answer);
-//-----------------//-----------------//-----------------//-----------------
-
-// 프로그래머스에 답변 제출
-
-    function solution(a, b) {
-        var answer = 0;
-        if (a === b) {
-                answer = a;
-        } else {
-                answer = a + b
-        }
-        return answer;  
-    }
+  //Object [Array Iterator] {}
